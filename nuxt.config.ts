7a415e0a02dcad1212d16987a6e6871369d71547
medusiora <khom@outsourcify.net>
2024-12-18 18:20:56 +0700

    feat: Add Pinia store for posts and related types, and implement API operations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxtjs/color-mode",
-    "shadcn-nuxt",
-    "@nuxt/eslint",
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    'shadcn-nuxt',
+    '@pinia/nuxt',
+    '@nuxt/eslint',
   ],
   shadcn: {
     /**
      * Prefix for all the imported component
      */
-    prefix: "",
+    prefix: '',
     /**
      * Directory that the component lives in.
      * @default "./components/ui"
      */
-    componentDir: "./components/ui",
+    componentDir: './components/ui',
   },
-});
+})
