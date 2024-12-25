daadb090d318a7e1694674389de4ce16fce6911d
medusiora <khom@outsourcify.net>
2024-12-25 17:39:21 +0700

    feat: update ESLint configuration to allow explicit any and unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,15 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt({
-  ignores: ['components/ui/**/*'],
-})
+export default withNuxt([
+  {
+    files: ['**/*.{js,ts,vue}'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
+  {
+    ignores: ['components/ui/**/*'],
+  },
+])
